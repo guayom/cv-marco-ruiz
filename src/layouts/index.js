89f61f7972e9b7d32443ce65bae9b52dd0755f4e
guayom <guayo.mena@gmail.com>
2018-08-02 17:05:47 -0600

    Use gatsby favicon plugin

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -13,17 +13,6 @@ const TemplateWrapper = ({ children }) => (
         { name: "msapplication-TileColor", content: "#46464f" },
         { name: "theme-color", content: "#ffffff" }
       ]}
-      link={[
-        {rel:"apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png"},
-        { rel: "icon", type: "image/png", sizes: "192x192", href: "/android-icon-192x192.png"},
-        { rel: "icon", type: "image/png", sizes: "96x96", href: "/favicon-96x96.png"},
-        {rel:"icon", type: "image/png", sizes:"32x32", href:"/favicon-32x32.png"},
-        {rel:"icon", type:"image/png", sizes:"16x16", href:"/favicon-16x16.png"},
-        {rel:"manifest", href:"/site.webmanifest"},
-        {rel:"mask-icon", href:"/safari-pinned-tab.svg", color:"#46464f"},
-        {rel:"shortcut icon", href: "/favicon-16x16.png", type: "image/x-icon"},
-        {rel:"icon", href: "/favicon-16x16.png", type: "image/x-icon" }
-      ]}
     />
     {children()}
   </div>
