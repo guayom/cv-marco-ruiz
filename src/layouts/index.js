28c43f9adf9a6f58d4b0688ea6396b27fab3adaa
guayom <guayo.mena@gmail.com>
2018-08-02 16:44:26 -0600

    Add more favicons

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -15,16 +15,19 @@ const TemplateWrapper = ({ children }) => (
       ]}
       link={[
         {rel:"apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png"},
+        { rel: "icon", type: "image/png", sizes: "192x192", href: "/android-icon-192x192.png"},
+        { rel: "icon", type: "image/png", sizes: "96x96", href: "/favicon-96x96.png"},
         {rel:"icon", type: "image/png", sizes:"32x32", href:"/favicon-32x32.png"},
         {rel:"icon", type:"image/png", sizes:"16x16", href:"/favicon-16x16.png"},
         {rel:"manifest", href:"/site.webmanifest"},
-        {rel:"mask-icon", href:"/safari-pinned-tab.svg", color:"#46464f"}
+        {rel:"mask-icon", href:"/safari-pinned-tab.svg", color:"#46464f"},
+        {rel:"shortcut icon", href: "/favicon-16x16.png", type: "image/x-icon"},
+        {rel:"icon", href: "/favicon-16x16.png", type: "image/x-icon" }
       ]}
     />
     {children()}
   </div>
 )
-
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
