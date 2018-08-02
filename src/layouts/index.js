6a50e44baf4ef40a7bb14be6b21164a11f41ea3c
guayom <guayo.mena@gmail.com>
2018-08-02 16:32:55 -0600

    Add favicon

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -10,11 +10,21 @@ const TemplateWrapper = ({ children }) => (
       title="Eduardo Mena"
       meta={[
         { name: 'description', content: 'Eduardo Mena professional profile' },
+        { name: "msapplication-TileColor", content: "#46464f" },
+        { name: "theme-color", content: "#ffffff" }
+      ]}
+      link={[
+        {rel:"apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png"},
+        {rel:"icon", type: "image/png", sizes:"32x32", href:"/favicon-32x32.png"},
+        {rel:"icon", type:"image/png", sizes:"16x16", href:"/favicon-16x16.png"},
+        {rel:"manifest", href:"/site.webmanifest"},
+        {rel:"mask-icon", href:"/safari-pinned-tab.svg", color:"#46464f"}
       ]}
     />
     {children()}
   </div>
 )
+
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
